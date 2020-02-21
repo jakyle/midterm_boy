f52a820ff4cee9c65105f6d329326dcfe43ac4ef
Jackson, James <jamesjackson@quickenloans.com>
2020-02-20 21:21:10 -0500

    add validition

---
+++ ConsoleApp1/Validation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class Validation
+    {
+        public static bool IsYesOrNo(string input)
+        {
+            return input.Equals("yes", StringComparison.OrdinalIgnoreCase)
+                 || input.Equals("no", StringComparison.OrdinalIgnoreCase);
+            
+        }
+    }
+}
